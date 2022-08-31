0df80b92d8d9bc5700ff27205bcb3af514e68dbd
m1sho <popwizardarraki@gmail.com>
2022-08-30 19:02:28 -0700

    changed language for word to kazakh (gui WIP)

--- WoTD.py
+++ WoTD.py
@@ -1,33 +1,99 @@
+import tkinter
 
-import bs4 as bs
+import translators as ts
 import random
+from tkinter import *
+import tkinter.messagebox
 from libretranslatepy import LibreTranslateAPI
-class menu:
-    lt = LibreTranslateAPI("https://translate.argosopentech.com/")
+
+kz= str(random.choice(open("kz.txt").read().split()))
 
 
+new_Word =False
 
-    lang_Choice = input("choose a language: \n Bulgarian \n Russian\n Kazakh ")
-    if lang_Choice=="bg" or "Bulgarian":
-        s= str (print(random.choice(open("bg_50k.txt").read().split())))
 
-    print(lt.translate(s, "bg", "en"))
-    
- class translate:
-    url = 'https://translate.google.ca/#zh-CN/en/%E6%B2%BB%E5%85%B7'
-res = requests.get(url)
-res.raise_for_status
-soup = bs4.BeautifulSoup(res.text, "html.parser")
-translation = soup.select('#result_box span')
-print(translation)
+langChoice=kz
 
 
 
 
 
 
+class Window(Frame):
 
-# add choice to view word in context
+     langChoice = ()
+
+     def __init__(self, master=None):
+
+          Frame.__init__(self, master)
+          self.master = master
+
+          # widget can take all window
+          self.pack(fill=BOTH, expand=1)
+
+
+          l = Label(self, text=kz)
+          l.config(font=("Courier", 14))
+          l.pack()
+
+
+
+
+          randWordBtn= Button(self, text="Translation/Defintion", command=self.onClick)
+          randWordBtn.place(x=0,y=0)
+          randWordBtn.pack()
+
+          langbutton = Button(self, text="Save to excel", command=self.onClick)
+          langbutton.place(x=0, y=0)
+          langbutton.pack()
+          langChoice=kz
+
+         
+
+          # create button, link it to clickExitButton()
+          exitButton = Button(self, text="Exit", command=self.clickExitButton)
+
+          # place button at (0,0)
+          exitButton.pack()
+
+
+
+     def clickExitButton(self):
+          exit()
+
+     def onClick(self):
+          if langChoice==kz:
+
+               tkinter.messagebox.showinfo("Definition",(ts.google(kz, from_language='kk',to_language='en')))
+
+
+
+
+
+
+
+
+root = Tk()
+
+
+app = Window(root)
+root.wm_title("Kazakh Word of The Day")
+root.geometry("320x200")
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
